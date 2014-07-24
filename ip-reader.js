d12f04441cb5bc3e2935da9d745d6618792a0e2a
Jeff Hann <jeffhann@gmail.com>
2014-07-24 11:06:31 -0300

    fixing things;wq

--- ip-reader.js
+++ ip-reader.js
@@ -1,13 +1,12 @@
 if (process.env.REDISTOGO_URL) {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis"),
-        client = redis.createClient(rtg.port, rtg.hostname);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
     redis.auth(rtg.auth.split(":")[1]);
 } else {
-    var redis = require("redis"),
-        client = redis.createClient();
+    var redis = require("redis").createClient();
 }
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -16,25 +15,25 @@ app.use(bodyParser());
 
 app.post('/', function(req, res){
     var data = req.body;
-    client.get("current_ip", function(err, reply){
+    redis.get("current_ip", function(err, reply){
         if(reply) {
-            client.set("old_ip", reply, redis.print);
+            redis.set("old_ip", reply, redis.print);
         }
 
-        client.set("current_ip", data.ip, redis.print);
+        redis.set("current_ip", data.ip, redis.print);
     });
 
     res.json({success: true});
 });
 
 app.get('/old', function(req, res){
-    client.get("old_ip", function(err, reply){
+    redis.get("old_ip", function(err, reply){
         res.send(reply);
     });
 });
 
 app.get('/', function(req, res){
-    client.get("current_ip", function(err, reply){
+    redis.get("current_ip", function(err, reply){
         res.send(reply);
     });
 });
